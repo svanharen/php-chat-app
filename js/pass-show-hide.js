d12bc9f60d1f32fc229a12840e06f1800c8e215a
Sam VH <svanharen@my.bcit.ca>
2023-08-03 11:41:18 -0700

    password toggle functional

--- js/pass-show-hide.js
+++ js/pass-show-hide.js
@@ -1,2 +1,24 @@
-const passwordField = document.querySelector(".form .field input[type='password']")
+// Inital Password Field
+const passField = document.querySelector(".form .field input[type='password']")
 toggleBtn = document.querySelector(".form .field i")
+
+toggleBtn.onclick = () => {
+    if(passField.type == "password"){
+        passField.type = "text";
+    }else{
+        passField.type = "password";
+    }
+}
+
+// Confirm Password Field
+const confirmPassField = document.querySelector(".form .cpw input[type='password']")
+toggleBtn2 = document.querySelector(".form .cpw i")
+
+toggleBtn2.onclick = () => {
+    if(confirmPassField.type == "password"){
+        confirmPassField.type = "text";
+    }else{
+        confirmPassField.type = "password";
+    }
+}
+
